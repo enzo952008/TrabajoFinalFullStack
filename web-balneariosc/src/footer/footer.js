fab2c1d71a20841410794759bc40a6fae733bcd3
enzo952008 <enzo952008@hotmail.com>
2024-05-23 15:15:01 -0300

    footer

---
+++ web-balneariosc/src/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css';
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className="footer">
+                <p>TODOS LOS DERECHOS RESERVADOS | MUNICIPALIDAD SC.</p>
+            </div>
+        );
+    }
+}
+
+export default Footer;

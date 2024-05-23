8390c495531c506727799e6f18d41f50d824231a
enzo952008 <enzo952008@hotmail.com>
2024-05-23 14:54:45 -0300

    fusion navbar con carrusel

--- web-balneariosc/src/App.js
+++ web-balneariosc/src/App.js
@@ -1,12 +1,16 @@
+// src/App.js
 import './App.css';
 import Carousel from './carousel/Carousel-component';
+import NavBar from './navbar/navBar';
 
 function App() {
   return (
     <div>
-    <Carousel></Carousel>
+      <NavBar />
+      <Carousel />
     </div>
   );
 }
 
 export default App;
+

5a904a2c686bdc5c89313d021806e2ab1dfd2bd6
enzo952008 <enzo952008@hotmail.com>
2024-06-17 19:38:08 -0300

    modificaciones en fotos

--- web-balneariosc/src/home/atraccionesturisticas/TouristAttractions-component.jsx
+++ web-balneariosc/src/home/atraccionesturisticas/TouristAttractions-component.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import './TouristAttractions.css'; // Asegúrate de crear este archivo CSS
 import imghome from './imgAttractions/barco.JPG'
+import refugio from './imgAttractions/refugio.png'
 
 
 const TouristAttractions = () => {
     return (
         <div className="tourist-attractions">
-            <h1>ATRACCIONES TURÍSTICAS</h1>
+            <h1>Atracciones turisticas</h1>
             <div className="cards-container">
                 <div className="card">
                     <div className="card-content">
-                        <p className="description">Descripción de la imagen 1</p>
+                        <p className="description">Pagrus</p>
                         <img src={imghome} alt="Imagen 1" />
                     </div>
                 </div>
                 <div className="card">
                     <div className="card-content">
-                        <p className="description">Descripción de la imagen 66</p>
-                        <img src={imghome} alt="Imagen 2" />
+                        <p className="description">El Refugio</p>
+                        <img src={refugio} alt="Imagen 2" />
                     </div>
                 </div>
             </div>

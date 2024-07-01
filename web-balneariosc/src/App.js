0588f6b8ec03c3ffd5d66958112e5acc488439c2
enzo952008 <enzo952008@hotmail.com>
2024-07-01 15:05:41 -0300

    ruteo de clima en app

--- web-balneariosc/src/App.js
+++ web-balneariosc/src/App.js
@@ -14,6 +14,7 @@ export default function App() {
   return (
     <BrowserRouter>
     <Routes>
+    <Route path='clima' element={<Windguru/>}></Route>
       <Route path='/' element={<Layout/>}></Route>
       <Route index element={<Home/>}></Route>
       <Route path='login' element={<Login/>}></Route>

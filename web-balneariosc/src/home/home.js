9de3397661bd92602f578d0c55528bc3372f25c2
enzo952008 <enzo952008@hotmail.com>
2024-07-01 00:10:50 -0300

    pull de clima

--- web-balneariosc/src/home/home.js
+++ web-balneariosc/src/home/home.js
@@ -4,7 +4,7 @@ import NavBar from '../components/navBar-component';
 import Carousel from '../components/Carousel-component';
 import DescriptionTourist from '../components/description-component';
 import TouristAttractions from '../components/TouristAttractions-component';
-import Footer from '../components/footer-component';
+import Footer from '../components/footer-component'; 
 import Login from '../login/login';
 // import Clima from '../components/clima-component';
 import { Route, Routes,} from 'react-router-dom';
@@ -15,12 +15,12 @@ class Home extends React.Component {
             <div>
                 <NavBar />
                 <Routes>
-                     {/* <Route path="/clima" component={Clima} />
-                    <Route path="/hospedaje" component={Hospedaje} />
+                    <Route path="/clima" component={Windguru} />
+                    {/* <Route path="/hospedaje" component={Hospedaje} />
                     <Route path="/gastronomia" component={Gastronomia} />
                     <Route path="/aguasdelpinar" component={AguasDelPinar} />
                     <Route path="/contacto" component={Contacto} />
-                    <Route path="/profile" component={Profile} />  */}
+                    <Route path="/profile" component={Profile} /> */}
                     <Route path="/login" component={Login} /> 
                 </Routes>
                 <Carousel />

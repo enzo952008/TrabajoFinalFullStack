56d3689236666065d138ea7ab3ddbcfb8497eb44
enzo952008 <enzo952008@hotmail.com>
2024-10-17 18:30:22 -0300

    ajustes en tarjetas y Aguas del pinar=> tickets terminado

--- web-balneariosc/src/components/ticket-component.jsx
+++ web-balneariosc/src/components/ticket-component.jsx
@@ -1,116 +1,132 @@
 import React, { useState, useEffect } from 'react';
-import TicketSelector from './ticket-selector-component';
-import '../styles/ticket.css';
+import TicketSelector from './ticket-selector-component'; // Asegúrate de importar tu componente de selección de tickets
+import '../styles/ticket.css'; // Archivo CSS
 
-// Array con los nombres de los días de la semana en español
- const diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+const diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
 
- const TicketBooking = () => {
-   const [selectedDate, setSelectedDate] = useState(null);
-   const [dates, setDates] = useState([]);
-   const [modalIsOpen, setModalIsOpen] = useState(false); // Estado del modal
+const TicketBooking = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [dates, setDates] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false); // Estado del modal
 
-   useEffect(() => {
-     const today = new Date();
-     const currentDay = today.getDay();
-     const dayMap = [2, 3, 4, 5, 6, 0]; // Mapeo de los días de la semana: Martes = 2, Miércoles = 3, ..., Domingo = 0
-     const daysToAdd = dayMap.map(day => (day - currentDay + 7) % 7);
+  useEffect(() => {
+    const today = new Date();
+    const currentDay = today.getDay();
+    const dayMap = [2, 3, 4, 5, 6, 0]; // Mapeo de los días de la semana
+    const daysToAdd = dayMap.map(day => (day - currentDay + 7) % 7);
 
-     // Calcular las nuevas fechas a partir de hoy
-     const newDates = daysToAdd.map(days => {
-       const date = new Date(today);
-       date.setDate(today.getDate() + days);
-       return date;
-     });
+    // Calcular las nuevas fechas
+    const newDates = daysToAdd.map(days => {
+      const date = new Date(today);
+      date.setDate(today.getDate() + days);
+      return date;
+    });
 
-     setDates(newDates); // Actualizar el estado con las nuevas fechas
-   }, []);
+    setDates(newDates); // Actualizar las fechas
+  }, []);
 
-   // Función para manejar el clic en una fecha
-   const handleDateClick = (date) => {
-     setSelectedDate(date); // Establecer la fecha seleccionada en el estado
-   };
+  // Manejar clic en una fecha
+  const handleDateClick = (date) => {
+    setSelectedDate(date); 
+  };
 
-   // Función para manejar el clic en el botón de comprar tickets
-   const handleBuyTicketClick = (date) => {
-     setSelectedDate(date); // Establecer la fecha seleccionada en el estado
-     setModalIsOpen(true); // Abrir el modal
-   };
+  // Manejar clic en "Comprar"
+  const handleBuyTicketClick = (date) => {
+    setSelectedDate(date); 
+    setModalIsOpen(true); // Abrir el modal
+  };
 
-   // Función para formatear la fecha en un formato legible
-    const formatDate = (date) => {
-    const dayName = diasSemana[date.getDay()];
-     return `${dayName}, ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-   };
-   const closeModal = () => setModalIsOpen(false);
+  // Formatear la fecha
+  const formatShortDate = (date) => {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = String(date.getFullYear()).slice(-2); 
+    return `${day}/${month}/${year}`;
+  };
+
+  const closeModal = () => setModalIsOpen(false); // Función para cerrar el modal
 
   return (
     <div className="ticket-booking">
-      <div className="header">
-        <h1>Días y horarios:</h1>
-        <p>Martes a Domingos de 11:00 a 19:00 hs</p>
-      </div>
       <div className="content-container">
-        <div className="compraTicket">
-          <h2>SEGUI ESTOS PASOS PARA COMPRAR TU TICKET: </h2>
-          <ol className="compraTabla">
-            <tbody>
-              <tr>
-                <li>Completa tus datos para identificarte. </li>
-              </tr>
-              <tr>
-                <li>Selecciona la fecha, cantidad y tipo de entradas que desea adquirir.</li>
-              </tr>
-              <tr>
-                <li>Haz click en siguiente y selecciona la forma de pago.</li>
-              </tr>
-              <tr>
-                <li>Una vez que Mercadopago confirme tu compra, le llegara un mail notificandole que la misma fue procesada
-                  satisfactoriamente y luego se le enviara el acceso al complejo. </li>
-              </tr>
-            </tbody>
-          </ol>
-        </div>
-        <div className="tariff-card">
-          <h2>TARIFAS</h2>
-          <table className="tariff-table">
-            <tbody>
-              <tr>
-                <td>Menores de 13 años</td>
-                <td>s/cargo</td>
-              </tr>
-              <tr>
-                <td>Personas con carnet CUD</td>
-                <td>s/cargo</td>
-              </tr>
-              <tr>
-                <td>Residentes</td>
-                <td>$500</td>
-              </tr>
-              <tr>
-                <td>Jubilados y pensionados</td>
-                <td>$500</td>
-              </tr>
-              <tr>
-                <td>No residentes</td>
-                <td>$2.000</td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="background-section">
+          <div className="header">
+            <h1>Días y horarios:</h1>
+            <p>Martes a Domingos de 11:00 a 19:00 hs</p>
+          </div>
+          <div className="compraTicket">
+            <h2>SEGUÍ ESTOS PASOS PARA COMPRAR TU TICKET:</h2>
+            <ol className="compraTabla">
+              <li>Completa tus datos para identificarte.</li>
+              <li>Selecciona la fecha, cantidad y tipo de entradas que deseas adquirir.</li>
+              <li>Haz clic en siguiente y selecciona la forma de pago.</li>
+              <li>
+                Una vez que MercadoPago confirme tu compra, te llegará un mail notificando que la misma fue procesada satisfactoriamente, y luego se enviará el acceso al complejo.
+              </li>
+            </ol>
+          </div>
+          <div className="tariff-card">
+            <h2>TARIFAS</h2>
+            <table className="tariff-table">
+              <tbody>
+                <tr>
+                  <td>Menores de 13 años</td>
+                  <td>s/cargo</td>
+                </tr>
+                <tr>
+                  <td>Personas con carnet CUD</td>
+                  <td>s/cargo</td>
+                </tr>
+                <tr>
+                  <td>Residentes</td>
+                  <td>$500</td>
+                </tr>
+                <tr>
+                  <td>Jubilados y pensionados</td>
+                  <td>$500</td>
+                </tr>
+                <tr>
+                  <td>No residentes</td>
+                  <td>$2.000</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
 
+        <h2 className="ticket-selection-title">Elegí el día y comprá tu entrada online</h2>
+
+        <div className="cards-container">
+          {dates.map((date, index) => (
+            <div className="ticket-card" key={index}>
+              <h3>Compra tu ticket</h3>
+              <p>{formatShortDate(date)}</p>
+              <img 
+                src="/static/media/tickets.ec77a841237a2b5bc437.png" 
+                alt="Imagen del ticket" 
+                className="ticket-image" 
+              />
+              <button className="buy-ticket-button" onClick={() => handleBuyTicketClick(date)}>
+                Comprar
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
-      
+
+      {/* Modal */}
       {modalIsOpen && (
-        <>
-          {selectedDate && <p>Fecha seleccionada: {formatDate(selectedDate)}</p>}
-          <TicketSelector isOpen={modalIsOpen} closeModal={closeModal} selectedDate={selectedDate} />
-        </>
+        <div className="modal-overlay">
+          <div className="modal-content">
+            {selectedDate && <p>Fecha seleccionada: {formatShortDate(selectedDate)}</p>}
+            <TicketSelector isOpen={modalIsOpen} closeModal={closeModal} selectedDate={selectedDate} />
+            <button onClick={closeModal}>Cerrar</button>
+          </div>
+        </div>
       )}
     </div>
-
   );
-}
-
+};
 
 export default TicketBooking;
+
